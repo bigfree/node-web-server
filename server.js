808f7576359335da001ca9f9eef032094f118035
Adam Miko <okukster@gmail.com>
2017-06-06 21:44:36 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// loger
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err)
+            console.log('Unable to append to server.log');
+    });
+
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintence.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my Home page!'
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        welcomeMessage: 'Some Page'
+    });
+});
+
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Nieco sa paradne dojebalo!'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
